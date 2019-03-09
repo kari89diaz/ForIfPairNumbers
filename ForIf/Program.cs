0c3fb5b99243377a4cfcb8f0a6512d4ad5664789
kari89diaz <kari89diaz@gmail.com>
2019-03-09 20:12:52 -0300

    If inside for to differentiate between pair numbers.

---
+++ ForIf/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 0; num < 11; num++) //no me dejo poner num = 10
+            {
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine(num + "Es par");
+                }
+                else
+                {
+                    Console.WriteLine(num + "Es impar");
+                }
+            }
+        }
+    }
+}
